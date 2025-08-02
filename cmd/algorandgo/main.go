4c539dcf2e27752b735e7aabf902c888fbfc125a
Gary <62193371+gary111868@users.noreply.github.com>
2025-08-03 02:01:40 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/algorandgo/main.go
@@ -0,0 +1,23 @@
+// cmd/algorandgo/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"algorandgo/internal/algorandgo"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := algorandgo.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
